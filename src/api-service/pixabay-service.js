0c251d90c36b97a7af418dd897b3bc4e9b60ff0c
Krasnonos <andreykrasnonos@icloud.com>
2022-05-18 20:51:59 +0300

    finish wright markup

--- src/api-service/pixabay-service.js
+++ src/api-service/pixabay-service.js
@@ -3,13 +3,13 @@ const axios = require('axios');
 export default class fetchByName {
   constructor() {
     this.page = 1;
-    this.queryValue = 'cat';
+    this.value = 'cat';
   }
 
   async fetchDataFromPixabay() {
     const BASIC_URL = 'https://pixabay.com/api/';
     const URL_KEY = '27491593-aa922f21d022df769349f5779';
-    const queryString = `q=${this.queryValue}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=40`;
+    const queryString = `q=${this.value}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=40`;
 
     try {
       return await axios.get(`${BASIC_URL}?key=${URL_KEY}&${queryString}`);
@@ -18,12 +18,8 @@ export default class fetchByName {
     }
   }
 
-  get queryValue() {
-    return this.queryValue;
-  }
-
-  set queryValue(newValue) {
-    this.queryValue = newValue;
+  queryValue(newValue) {
+    this.value = newValue;
   }
 
   increasePage() {
--- src/js/index.js
+++ src/js/index.js
@@ -5,18 +5,43 @@ import imgTemplate from '../hbs/img-template.hbs';
 import fetchApiByName from '../api-service/pixabay-service';
 
 const API = new fetchApiByName();
-console.log(API);
 
 const refs = {
   form: document.querySelector('.search-form'),
+  gallery: document.querySelector('.gallery'),
+  showMoreBtn: document.querySelector('.load-more '),
 };
 
 refs.form.addEventListener('submit', onSearchForm);
+refs.showMoreBtn.addEventListener('click', onShowMore);
 
 async function onSearchForm(e) {
   e.preventDefault();
 
-  const queryValue = e.currentTarget.elements.searchQuery.value;
+  const inputValue = e.currentTarget.elements.searchQuery.value;
+
+  if (inputValue === '') {
+    console.log('empty form');
+    return;
+  }
+
+  API.queryValue(inputValue);
+
   const result = await API.fetchDataFromPixabay();
-  console.log(result);
+  resetMarkup();
+  reWrightMarkup(result);
+}
+
+async function onShowMore() {
+  API.increasePage();
+  const result = await API.fetchDataFromPixabay();
+  reWrightMarkup(result);
+}
+
+function reWrightMarkup(markup) {
+  refs.gallery.insertAdjacentHTML('beforeend', imgTemplate(markup));
+}
+
+function resetMarkup() {
+  refs.gallery.innerHTML = '';
 }
